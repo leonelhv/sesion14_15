feee03298966a5c8d4c7ac9bdd15eb2826928904
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-10 10:04:46 -0500

    export component hijos14component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HijoS14Component } from './hijo-s14/hijo-s14.component';
+
+@NgModule({
+  declarations: [HijoS14Component],
+  imports: [CommonModule],
+  exports: [HijoS14Component],
+})
+export class ComponentsModule {}
