056ced6400d99d269992f63ccae0e6a4f5110843
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-10 10:29:26 -0500

    finish s15 component

--- src/app/pages/home-s15/home-s15.component.ts
+++ src/app/pages/home-s15/home-s15.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-s15',
   templateUrl: './home-s15.component.html',
-  styleUrls: ['./home-s15.component.css']
+  styleUrls: ['./home-s15.component.css'],
 })
 export class HomeS15Component {
+  titulo = 'Data Binding Interpolation';
+  isVisible = true;
+  textLarge =
+    'El Fvo-way data bindtng en Angular nos pennite modificar el valor de una propiedad de forma bidireccional, de forma que cuando nosotros modificarnos el valor de una propiedad en la vista el valor tarnbién se actualiza en el modelo de datos.';
 
+  onVisible() {
+    this.isVisible = !this.isVisible;
+  }
 }
