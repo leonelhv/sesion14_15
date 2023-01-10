83c5ae9bfa9d472a2ae0ac9f86f8ad9145e1ed8e
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-10 10:07:06 -0500

    add home-s15 component

---
+++ src/app/pages/home-s15/home-s15.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-s15',
+  templateUrl: './home-s15.component.html',
+  styleUrls: ['./home-s15.component.css']
+})
+export class HomeS15Component {
+
+}
