c1dba88fe572573f9aa4ec7d6553cde1973008c1
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-10 10:03:35 -0500

    add path routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeS14Component } from './pages/home-s14/home-s14.component';
+import { HomeS15Component } from './pages/home-s15/home-s15.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home-s14',
+    component: HomeS14Component,
+  },
+  {
+    path: 'home-s15',
+    component: HomeS15Component,
+  },
+  {
+    path: '**',
+    redirectTo: 'home-s14',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
